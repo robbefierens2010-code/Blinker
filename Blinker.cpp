f68626af05a3ed206130c017e776776a5bcc8a94
robbefierens2010-code <robbefierens2010@gmail.com>
2026-09-24 09:41:46 +0200

    Update Blinker.cpp

--- Blinker.cpp
+++ Blinker.cpp
@@ -10,7 +10,7 @@ Blinker::Blinker(int pin, unsigned long interval , unsigned long startvertaging 
     pinMode(_pin, OUTPUT);
 }
 
-int Blinker::kweeni(void){
+int Blinker::handle(void){
     unsigned long nu = millis();
     int retval = false;
 
